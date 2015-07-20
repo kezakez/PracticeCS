9d4557ade88b10c82bb12f0861741d7dac0a3362
Keiran McDonald <keza@keza.net>
2015-07-20 22:38:20 +1000

    Added character at position problem.

---
+++ PracticeCS.Tests/CharacterAtPostionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace PracticeCS.Tests
+{
+    [TestClass]
+    public class CharacterAtPostionTests
+    {
+        [TestMethod]
+        public void CharacterAtPostion_should_ReturnValidResults()
+        {
+            var target = new CharacterAtPosition.CharacterAtPosition();
+
+            target.GetCharacter(0).ShouldEqual('0');
+
+            target.GetCharacter(11).ShouldEqual('0');
+
+            target.GetCharacter(1).ShouldEqual('1');
+
+            target.GetCharacter(24).ShouldEqual('1');
+
+            target.GetCharacter(33).ShouldEqual('1');
+
+            target.GetCharacter(23).ShouldEqual('6');
+
+            target.GetCharacter(9999).ShouldEqual('7');
+        }
+    }
+}
---
+++ PracticeCS/CharacterAtPosition/CharacterAtPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PracticeCS.CharacterAtPosition
+{
+    /// <summary>
+    /// Numbers are serialized increasingly into a sequence in the format of 0123456789101112131415,
+    /// each digit occupies a position in the sequence. For instance, the digit in position 5 is 5, in position 13 is 1, in the position 19 is 4, and so on.
+    /// Please write a function/method to get the digit for any given position.
+    /// </summary>
+    public class CharacterAtPosition
+    {
+        public char GetCharacter(int position)
+        {
+            var counter = 0;
+            for (var current = 0; current <= position; current++)
+            {
+                var stringToAppend = current.ToString();
+                if (counter == position)
+                {
+                    return stringToAppend[0];
+                }
+                counter += stringToAppend.Length;
+                if (counter > position)
+                {
+                    return stringToAppend[stringToAppend.Length - (counter - position)];
+                }
+            }
+            throw new InvalidOperationException("character position not valid");
+        }
+    }
+}
